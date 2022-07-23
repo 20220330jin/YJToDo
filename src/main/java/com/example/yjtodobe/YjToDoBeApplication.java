68e8b0c39f7708e708366e69208a2d532012470e
jin <20220330jin@gmail.com>
2022-07-23 17:53:05 +0900

    [추가] BE 프로젝트 생성

---
+++ src/main/java/com/example/yjtodobe/YjToDoBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.yjtodobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YjToDoBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YjToDoBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/yjtodobe/YjToDoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yjtodobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YjToDoBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
